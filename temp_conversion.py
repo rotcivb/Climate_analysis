0db4571f8841b06785aaf324cb7f55dbf8e7e2ae
rotcivb <75248677+rotcivb@users.noreply.github.com>
2023-08-02 12:20:53 +0100

    Update temp_conversion.py

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,10 +1,33 @@
 # Todo: Code is a bit unclear
 
-def Fc(x):
-    Y = (x - 32) * (5 / 9)
-    return Y
+def fahrenheit_to_celsius(fahr):
 
-def FK(x):
-    y = Fc(x)
-    z = y + 273.15
-    return z
+    """
+    Given temp in fahrebheit, converts it to celsius
+
+    :parm fahr: Temperature in Fahrenheit
+    :return: The temperature in Celsius
+    """
+    
+    celsius = (fahr - 32) * (5 / 9)
+    return celsius
+
+
+
+def fahrenheit_to_kelvin(fahr):
+    """
+    Given temp in fahrebheit, converts it to Kelvin
+
+    :parm fahr: Temperature in Fahrenheit
+    :raises ValueError: If the temperature is below absolute zero
+    :return: The temperature in Kelvin
+    """
+    
+    celsius = fahrenheit_to_celsius(fahr)
+    if celsius < -273.15:
+        # If temperature is les than absolute zero
+        raise ValueError(
+            f'Trying to convert impossible temperature: {fahr}F'
+        )
+    kelvin = celsius + 273.15
+    return kelvin
